e990ce1ca7ccacc5afdc3eacd80809ba1b8a8d95
Lera Chukovich <chukalera@gmail.com>
2020-12-04 21:17:44 +0100

    refactor: change file paths

--- src/cards.js
+++ src/cards.js
@@ -56,50 +56,50 @@ const cards = [
         {
             word: 'cat',
             translation: 'кот',
-            image: 'assets/images/cat.png',
-            audio: 'assets/audio/cat.mp3'
+            image: '.src/assets/images/cat.png',
+            audio: '.src/assets/audio/cat.mp3'
         },
         {
             word: 'horse',
             translation: 'лошадь',
-            image: 'assets/images/horse.png',
-            audio: 'assets/audio/horse.mp3'
+            image: '.src/assets/images/horse.png',
+            audio: '.src/assets/audio/horse.mp3'
         },
         {
             word: 'penguin',
             translation: 'пингвин',
-            image: 'assets/images/penguin.png',
-            audio: 'assets/audio/penguin.mp3'
+            image: '.src/assets/images/penguin.png',
+            audio: '.src/assets/audio/penguin.mp3'
         },
         {
             word: 'pig',
             translation: 'свинья',
-            image: 'assets/images/pig.png',
-            audio: 'assets/audio/pig.mp3'
+            image: '.src/assets/images/pig.png',
+            audio: '.src/assets/audio/pig.mp3'
         },
         {
             word: 'giraffe',
             translation: 'жираф',
-            image: 'assets/images/giraffe.png',
-            audio: 'assets/audio/giraffe.mp3'
+            image: '.src/assets/images/giraffe.png',
+            audio: '.src/assets/audio/giraffe.mp3'
         },
         {
             word: 'elephant',
             translation: 'слон',
-            image: 'assets/images/elephant.png',
-            audio: 'assets/audio/elephant.mp3'
+            image: '.src/assets/images/elephant.png',
+            audio: '.src/assets/audio/elephant.mp3'
         },
         {
             word: 'crocodile',
             translation: 'крокодил',
-            image: 'assets/images/crocodile.png',
-            audio: 'assets/audio/crocodile.mp3'
+            image: '.src/assets/images/crocodile.png',
+            audio: '.src/assets/audio/crocodile.mp3'
         },
         {
             word: 'cock',
             translation: 'петух',
-            image: 'assets/images/cock.png',
-            audio: 'assets/audio/cock.mp3'
+            image: '.src/assets/images/cock.png',
+            audio: '.src/assets/audio/cock.mp3'
         }
     ],
     [
@@ -107,50 +107,50 @@ const cards = [
         {
             word: 'calm',
             translation: 'спокойный',
-            image: 'assets/images/calm.png',
-            audio: 'assets/audio/calm.mp3'
+            image: '.src/assets/images/calm.png',
+            audio: '.src/assets/audio/calm.mp3'
         },
         {
             word: 'smile',
             translation: 'улыбка',
-            image: 'assets/images/smile.png',
-            audio: 'assets/audio/smile.mp3'
+            image: '.src/assets/images/smile.png',
+            audio: '.src/assets/audio/smile.mp3'
         },
         {
             word: 'happy',
             translation: 'счастливый',
-            image: 'assets/images/happy.png',
-            audio: 'assets/audio/happy.mp3'
+            image: '.src/assets/images/happy.png',
+            audio: '.src/assets/audio/happy.mp3'
         },
         {
             word: 'surprised',
             translation: 'удивленный',
-            image: 'assets/images/surprised.png',
-            audio: 'assets/audio/surprised.mp3'
+            image: '.src/assets/images/surprised.png',
+            audio: '.src/assets/audio/surprised.mp3'
         },
         {
             word: 'angry',
             translation: 'злой',
-            image: 'assets/images/angry.png',
-            audio: 'assets/audio/angry.mp3'
+            image: '.src/assets/images/angry.png',
+            audio: '.src/assets/audio/angry.mp3'
         },
         {
             word: 'fury',
             translation: 'ярость',
-            image: 'assets/images/fury.png',
-            audio: 'assets/audio/fury.mp3'
+            image: '.src/assets/images/fury.png',
+            audio: '.src/assets/audio/fury.mp3'
         },
         {
             word: 'cry',
             translation: 'плакать',
-            image: 'assets/images/cry.png',
-            audio: 'assets/audio/cry.mp3'
+            image: '.src/assets/images/cry.png',
+            audio: '.src/assets/audio/cry.mp3'
         },
         {
             word: 'scared',
             translation: 'напуган',
-            image: 'assets/images/scared.png',
-            audio: 'assets/audio/scared.mp3'
+            image: '.src/assets/images/scared.png',
+            audio: '.src/assets/audio/scared.mp3'
         }
     ],
     [
@@ -158,50 +158,50 @@ const cards = [
         {
             word: 'cooking',
             translation: 'кулинария',
-            image: 'assets/images/cooking.png',
-            audio: 'assets/audio/cooking.mp3'
+            image: '.src/assets/images/cooking.png',
+            audio: '.src/assets/audio/cooking.mp3'
         },
         {
             word: 'football',
             translation: 'футбол',
-            image: 'assets/images/football.png',
-            audio: 'assets/audio/football.mp3'
+            image: '.src/assets/images/football.png',
+            audio: '.src/assets/audio/football.mp3'
         },
         {
             word: 'gymnastics',
             translation: 'гимнастика',
-            image: 'assets/images/gymnastics.png',
-            audio: 'assets/audio/gymnastics.mp3'
+            image: '.src/assets/images/gymnastics.png',
+            audio: '.src/assets/audio/gymnastics.mp3'
         },
         {
             word: 'music',
             translation: 'музыка',
-            image: 'assets/images/music.png',
-            audio: 'assets/audio/music.mp3'
+            image: '.src/assets/images/music.png',
+            audio: '.src/assets/audio/music.mp3'
         },
         {
             word: 'painting',
             translation: 'рисование',
-            image: 'assets/images/painting.png',
-            audio: 'assets/audio/painting.mp3'
+            image: '.src/assets/images/painting.png',
+            audio: '.src/assets/audio/painting.mp3'
         },
         {
             word: 'reading',
             translation: 'чтение',
-            image: 'assets/images/reading.png',
-            audio: 'assets/audio/reading.mp3'
+            image: '.src/assets/images/reading.png',
+            audio: '.src/assets/audio/reading.mp3'
         },
         {
             word: 'science',
             translation: 'наука',
-            image: 'assets/images/science.png',
-            audio: 'assets/audio/science.mp3'
+            image: '.src/assets/images/science.png',
+            audio: '.src/assets/audio/science.mp3'
         },
         {
             word: 'singing',
             translation: 'пение',
-            image: 'assets/images/singing.png',
-            audio: 'assets/audio/singing.mp3'
+            image: '.src/assets/images/singing.png',
+            audio: '.src/assets/audio/singing.mp3'
         }
     ],
     [
@@ -209,50 +209,50 @@ const cards = [
         {
             word: 'grandfather',
             translation: 'дедушка',
-            image: 'assets/images/grandfather.png',
-            audio: 'assets/audio/grandfather.mp3'
+            image: '.src/assets/images/grandfather.png',
+            audio: '.src/assets/audio/grandfather.mp3'
         },
         {
             word: 'grandmother',
             translation: 'бабушка',
-            image: 'assets/images/grandmother.png',
-            audio: 'assets/audio/grandmother.mp3'
+            image: '.src/assets/images/grandmother.png',
+            audio: '.src/assets/audio/grandmother.mp3'
         },
         {
             word: 'father',
             translation: 'папа',
-            image: 'assets/images/father.png',
-            audio: 'assets/audio/father.mp3'
+            image: '.src/assets/images/father.png',
+            audio: '.src/assets/audio/father.mp3'
         },
         {
             word: 'mother',
             translation: 'мама',
-            image: 'assets/images/mother.png',
-            audio: 'assets/audio/mother.mp3'
+            image: '.src/assets/images/mother.png',
+            audio: '.src/assets/audio/mother.mp3'
         },
         {
             word: 'son',
             translation: 'сын',
-            image: 'assets/images/son.png',
-            audio: 'assets/audio/son.mp3'
+            image: '.src/assets/images/son.png',
+            audio: '.src/assets/audio/son.mp3'
         },
         {
             word: 'daughter',
             translation: 'дочь',
-            image: 'assets/images/daughter.png',
-            audio: 'assets/audio/daughter.mp3'
+            image: '.src/assets/images/daughter.png',
+            audio: '.src/assets/audio/daughter.mp3'
         },
         {
             word: 'baby',
             translation: 'малыш',
-            image: 'assets/images/baby.png',
-            audio: 'assets/audio/baby.mp3'
+            image: '.src/assets/images/baby.png',
+            audio: '.src/assets/audio/baby.mp3'
         },
         {
             word: 'pet',
             translation: 'питомец',
-            image: 'assets/images/pet.png',
-            audio: 'assets/audio/pet.mp3'
+            image: '.src/assets/images/pet.png',
+            audio: '.src/assets/audio/pet.mp3'
         }
     ],
     [
@@ -260,50 +260,50 @@ const cards = [
         {
             word: 'burger',
             translation: 'бургер',
-            image: 'assets/images/burger.png',
-            audio: 'assets/audio/burger.mp3'
+            image: '.src/assets/images/burger.png',
+            audio: '.src/assets/audio/burger.mp3'
         },
         {
             word: 'chicken',
             translation: 'курица',
-            image: 'assets/images/chicken.png',
-            audio: 'assets/audio/chicken.mp3'
+            image: '.src/assets/images/chicken.png',
+            audio: '.src/assets/audio/chicken.mp3'
         },
         {
             word: 'fries',
             translation: 'картофель фри',
-            image: 'assets/images/fries.png',
-            audio: 'assets/audio/fries.mp3'
+            image: '.src/assets/images/fries.png',
+            audio: '.src/assets/audio/fries.mp3'
         },
         {
             word: 'hot dog',
             translation: 'хот дог',
-            image: 'assets/images/hot-dog.png',
-            audio: 'assets/audio/hot-dog.mp3'
+            image: '.src/assets/images/hot-dog.png',
+            audio: '.src/assets/audio/hot-dog.mp3'
         },
         {
             word: 'pasta',
             translation: 'паста',
-            image: 'assets/images/pasta.png',
-            audio: 'assets/audio/pasta.mp3'
+            image: '.src/assets/images/pasta.png',
+            audio: '.src/assets/audio/pasta.mp3'
         },
         {
             word: 'salad',
             translation: 'салат',
-            image: 'assets/images/salad.png',
-            audio: 'assets/audio/salad.mp3'
+            image: '.src/assets/images/salad.png',
+            audio: '.src/assets/audio/salad.mp3'
         },
         {
             word: 'sandwich',
             translation: 'бутерброд',
-            image: 'assets/images/sandwich.png',
-            audio: 'assets/audio/sandwich.mp3'
+            image: '.src/assets/images/sandwich.png',
+            audio: '.src/assets/audio/sandwich.mp3'
         },
         {
             word: 'sausage',
             translation: 'колбаса',
-            image: 'assets/images/sausage.png',
-            audio: 'assets/audio/sausage.mp3'
+            image: '.src/assets/images/sausage.png',
+            audio: '.src/assets/audio/sausage.mp3'
         }
     ],
     [
@@ -311,50 +311,50 @@ const cards = [
         {
             word: 'broccoli',
             translation: 'брокколи',
-            image: 'assets/images/broccoli.png',
-            audio: 'assets/audio/broccoli.mp3'
+            image: '.src/assets/images/broccoli.png',
+            audio: '.src/assets/audio/broccoli.mp3'
         },
         {
             word: 'cabbage',
             translation: 'капуста',
-            image: 'assets/images/cabbage.png',
-            audio: 'assets/audio/cabbage.mp3'
+            image: '.src/assets/images/cabbage.png',
+            audio: '.src/assets/audio/cabbage.mp3'
         },
         {
             word: 'carrot',
             translation: 'морковь',
-            image: 'assets/images/carrot.png',
-            audio: 'assets/audio/carrot.mp3'
+            image: '.src/assets/images/carrot.png',
+            audio: '.src/assets/audio/carrot.mp3'
         },
         {
             word: 'corn',
             translation: 'кукуруза',
-            image: 'assets/images/corn.png',
-            audio: 'assets/audio/corn.mp3'
+            image: '.src/assets/images/corn.png',
+            audio: '.src/assets/audio/corn.mp3'
         },
         {
             word: 'eggplant',
             translation: 'баклажан',
-            image: 'assets/images/eggplant.png',
-            audio: 'assets/audio/eggplant.mp3'
+            image: '.src/assets/images/eggplant.png',
+            audio: '.src/assets/audio/eggplant.mp3'
         },
         {
             word: 'garlic',
             translation: 'чеснок',
-            image: 'assets/images/garlic.png',
-            audio: 'assets/audio/garlic.mp3'
+            image: '.src/assets/images/garlic.png',
+            audio: '.src/assets/audio/garlic.mp3'
         },
         {
             word: 'potato',
             translation: 'картофель',
-            image: 'assets/images/potato.png',
-            audio: 'assets/audio/potato.mp3'
+            image: '.src/assets/images/potato.png',
+            audio: '.src/assets/audio/potato.mp3'
         },
         {
             word: 'radish',
             translation: 'редиска',
-            image: 'assets/images/radish.png',
-            audio: 'assets/audio/radish.mp3'
+            image: '.src/assets/images/radish.png',
+            audio: '.src/assets/audio/radish.mp3'
         }
     ],
     [
@@ -362,50 +362,50 @@ const cards = [
         {
             word: 'banana',
             translation: 'банан',
-            image: 'assets/images/banana.png',
-            audio: 'assets/audio/banana.mp3'
+            image: '.src/assets/images/banana.png',
+            audio: '.src/assets/audio/banana.mp3'
         },
         {
             word: 'cherry',
             translation: 'вишня',
-            image: 'assets/images/cherry.png',
-            audio: 'assets/audio/cherry.mp3'
+            image: '.src/assets/images/cherry.png',
+            audio: '.src/assets/audio/cherry.mp3'
         },
         {
             word: 'grape',
             translation: 'виноград',
-            image: 'assets/images/grape.png',
-            audio: 'assets/audio/grape.mp3'
+            image: '.src/assets/images/grape.png',
+            audio: '.src/assets/audio/grape.mp3'
         },
         {
             word: 'mango',
             translation: 'манго',
-            image: 'assets/images/mango.png',
-            audio: 'assets/audio/mango.mp3'
+            image: '.src/assets/images/mango.png',
+            audio: '.src/assets/audio/mango.mp3'
         },
         {
             word: 'peach',
             translation: 'персик',
-            image: 'assets/images/peach.png',
-            audio: 'assets/audio/peach.mp3'
+            image: '.src/assets/images/peach.png',
+            audio: '.src/assets/audio/peach.mp3'
         },
         {
             word: 'pineapple',
             translation: 'ананас',
-            image: 'assets/images/pineapple.png',
-            audio: 'assets/audio/pineapple.mp3'
+            image: '.src/assets/images/pineapple.png',
+            audio: '.src/assets/audio/pineapple.mp3'
         },
         {
             word: 'pomegranate',
             translation: 'гранат',
-            image: 'assets/images/pomegranate.png',
-            audio: 'assets/audio/pomegranate.mp3'
+            image: '.src/assets/images/pomegranate.png',
+            audio: '.src/assets/audio/pomegranate.mp3'
         },
         {
             word: 'raspberry',
             translation: 'малина',
-            image: 'assets/images/raspberry.png',
-            audio: 'assets/audio/raspberry.mp3'
+            image: '.src/assets/images/raspberry.png',
+            audio: '.src/assets/audio/raspberry.mp3'
         }
     ],
     [
@@ -413,50 +413,50 @@ const cards = [
         {
             word: 'blue',
             translation: 'голубой',
-            image: 'assets/images/blue.png',
-            audio: 'assets/audio/blue.mp3'
+            image: '.src/assets/images/blue.png',
+            audio: '.src/assets/audio/blue.mp3'
         },
         {
             word: 'brown',
             translation: 'коричневый',
-            image: 'assets/images/brown.png',
-            audio: 'assets/audio/brown.mp3'
+            image: '.src/assets/images/brown.png',
+            audio: '.src/assets/audio/brown.mp3'
         },
         {
             word: 'blue',
             translation: 'синий',
-            image: 'assets/images/blue.png',
-            audio: 'assets/audio/blue.mp3'
+            image: '.src/assets/images/blue.png',
+            audio: '.src/assets/audio/blue.mp3'
         },
         {
             word: 'green',
             translation: 'зеленый',
-            image: 'assets/images/green.png',
-            audio: 'assets/audio/green.mp3'
+            image: '.src/assets/images/green.png',
+            audio: '.src/assets/audio/green.mp3'
         },
         {
             word: 'orange',
             translation: 'оранжевый',
-            image: 'assets/images/orange.png',
-            audio: 'assets/audio/orange.mp3'
+            image: '.src/assets/images/orange.png',
+            audio: '.src/assets/audio/orange.mp3'
         },
         {
             word: 'purple',
             translation: 'фиолетовый',
-            image: 'assets/images/purple.png',
-            audio: 'assets/audio/purple.mp3'
+            image: '.src/assets/images/purple.png',
+            audio: '.src/assets/audio/purple.mp3'
         },
         {
             word: 'red',
             translation: 'красный',
-            image: 'assets/images/red.png',
-            audio: 'assets/audio/red.mp3'
+            image: '.src/assets/images/red.png',
+            audio: '.src/assets/audio/red.mp3'
         },
         {
             word: 'yellow',
             translation: 'желтый',
-            image: 'assets/images/yellow.png',
-            audio: 'assets/audio/yellow.mp3'
+            image: '.src/assets/images/yellow.png',
+            audio: '.src/assets/audio/yellow.mp3'
         }
     ],
 ]
