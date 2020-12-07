60e6eec226fe17369db4cb0b14635cacf3718191
Lera Chukovich <chukalera@gmail.com>
2020-12-07 23:49:50 +0100

    feat: render application and menu

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import './styles/style.less';
 import './Menu';
-import './Card';
 import data from './data';
-import Card from "./Card";
-import Category from "./Category";
 import Application from './Application';
 
 window.addEventListener('load', function () {
-    return document.body.appendChild(new Application(data).render());
+    let App = new Application(data);
+     document.body.appendChild(App.render());
+     document.querySelector('.menu').appendChild(App.renderMenu())
 });
-
-
-
-
-// const newCard = new Card(data[1][0], true);
-// document.body.appendChild(newCard.create());
-
-//
-
-// const newCat = new Category(data[0][0], data[8]);
-// document.body.appendChild(newCat.render());
-
-// const newApp = new Application(data);
-// document.body.appendChild(newApp.render());
-
