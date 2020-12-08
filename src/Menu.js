80eff1f9fc3eb94b92dfd4c807af8a449ae068e4
Lera Chukovich <chukalera@gmail.com>
2020-12-08 11:52:29 +0100

    feat: add ability to close menu

--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,24 @@
 const menuIcon = document.querySelector('.menu__burger');
 const menu = document.querySelector('.menu');
 
-menuIcon.addEventListener('click', () => {
+const toggleMenu = function () {
   menu.classList.toggle('change');
+}
+
+menuIcon.addEventListener('click', (e) => {
+  e.stopPropagation()
+  toggleMenu();
 });
+
+document.addEventListener('click', function(e) {
+  const target = e.target;
+  const its_menu = target === menu || menu.contains(target);
+  const menu_is_active = menu.classList.contains('change');
+
+  if (!its_menu && menu_is_active) {
+    toggleMenu();
+    document.body.classList.toggle('');
+  }
+});
+
+
